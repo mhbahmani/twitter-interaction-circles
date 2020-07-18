b561ccaea2e239a1dcb4227b0e813b419771bcf7
mhbahmani <mhbahmani79@gmail.com>
2020-07-18 13:59:30 +0430

    add a test url

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ from flask import Flask
 
 app = Flask("twiiter-interaction-circle")
 
+@app.route('/')
+def test():
+    return 'Hello World!\n'
+
 @app.route('/<username>')
 def run(username):
     os.system('yarn build %s' % username)
     return 'Done!\n'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, host='0.0.0.0')
